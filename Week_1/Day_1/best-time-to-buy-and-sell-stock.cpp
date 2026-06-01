c2dd86f92c535f444725396d96bb3d6eb20730bd
sainiutkarsh94 <sainiutkarsh94@gmail.com>
2026-06-01 12:41:59 +0530

    Add files via upload

---
+++ Week_1/Day_1/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+int minPrice=prices[0];
+int maxProfit=0;
+for(int p:prices){
+    minPrice=min(minPrice,p);
+    maxProfit=max(maxProfit,p-minPrice);
+}   return maxProfit;
+    }
+};
